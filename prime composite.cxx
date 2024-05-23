dca91038b4a36c3ae08eb096c25936b1931fec3f
bauchajr1 <132862158+bauchajr1@users.noreply.github.com>
2024-05-23 09:30:56 +0545

    Update prime composite.cxx

--- prime composite.cxx
+++ prime composite.cxx
@@ -2,21 +2,11 @@
 #include <conio.h>
 int main()
 {
-	//variable declaration
 	int num, i, count=0,limit;
 	printf("Enter limit number: ");
-	//user enters the limit 
 	scanf("%d", &limit);
 	num=2;
-	//starting the loop until limit is reached
 	while(num<limit){
-	    /*
-	    checking if tge number  is prime or not
-	    -- with each contition being true 
-	        count is increased
-	        
-	      ->since prime has only 2 factorials(i.e can be only divided by itself and 1) count will only be 2
-	    */
 	for (i = 1; i <= num; i++)
 	{
 		if (num % i ==0)
@@ -25,7 +15,6 @@ int main()
 		}
 		
 	}
-//printing the num if it is prime
 	if (count ==2){
 	printf("%d\n",num);
 	}
